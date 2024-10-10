91e4dcee471fd28abdb06bc673ef5c680d392851
D3VI R9JPOOT <devixaayat@gmail.com>
2024-10-10 23:26:39 +0500

    Update devi.py

--- devi.py
+++ devi.py
@@ -68,7 +68,7 @@ def send_message():
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>𝑴𝑹 𝑫𝑬𝑽𝑰</title>
+	<title>Mr Devi</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 	<style>
 		body{
